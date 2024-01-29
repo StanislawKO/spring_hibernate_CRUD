6fc57bbb71c74c328a0d5fecf74cc03650e5b8f4
Stanislaav Kozh <printks@yandex.ru>
2024-01-29 18:48:05 +0300

    add method save, update, delete

--- src/main/java/com/kozh/springcourse/models/Person.java
+++ src/main/java/com/kozh/springcourse/models/Person.java
@@ -23,6 +23,11 @@ public class Person {
     @Column(name = "age")
     private int age;
 
+    @Email
+    @NotEmpty(message = "Email should not be empty")
+    @Column(name = "email")
+    private String email;
+
     public Person() {
 
     }
@@ -54,5 +59,23 @@ public class Person {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
